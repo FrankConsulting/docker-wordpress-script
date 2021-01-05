d74f26be01292d1120deb758fb80cb87d80ca9f9
dan-frank <dan.frank.lucas@gmail.com>
2021-01-05 08:29:31 +0000

    Replaced MAMP db with docker

--- newWebProject/newWebProject.sh
+++ newWebProject/newWebProject.sh
@@ -1,26 +1,32 @@
+# HELPFUL RESOURCES
+# Docker
+# - https://upcloud.com/community/tutorials/wordpress-with-docker/
+# - https://docs.docker.com/config/containers/container-networking/
+
 read -p "Enter your project name (no spaces): " PROJECT
 
-PROJECT_PATH="${HOME}/Documents/workspace/web/projects/${PROJECT}"
-WORDPRESS_TO_THEME="wp-content/themes"
+PATH_PROJECT="${HOME}/Documents/workspace/web/projects/${PROJECT}"
+PATH_WP="html/wp-content"
+PATH_WP_THEME="${PATH_WP}/themes"
+PATH_WP_PLUGINS="${PATH_WP}/plugins"
+PATH_SCRIPT="${HOME}/Documents/workspace/scripts/newWebProject"
 
-# PROCESS 
-open -j /Applications/MAMP/MAMP.app/
+mkdir ${PATH_PROJECT}
+cd ${PATH_PROJECT}
 
-mkdir ${PROJECT_PATH}
+docker run -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=wordpress --name ${PROJECT}_db -v "$PWD/database":/var/lib/mysql -d mariadb:latest
+docker run -e WORDPRESS_DB_PASSWORD=root --name ${PROJECT} --link ${PROJECT}_db:mysql -p 127.0.0.1:8080:80 -v "$PWD/html":/var/www/html -d wordpress
 
-git clone https://github.com/WordPress/WordPress.git ${PROJECT_PATH}
-cd ${PROJECT_PATH}/${WORDPRESS_TO_THEME}
-
-git clone https://github.com/dan-frank/swd_theme ${PROJECT}
-cd ${PROJECT_PATH}/${WORDPRESS_TO_THEME}/${PROJECT}
+git clone https://github.com/dan-frank/swd_theme ${PATH_PROJECT}/${PATH_WP_THEME}/${PROJECT}
+cd ${PATH_PROJECT}/${PATH_WP_THEME}/${PROJECT}
 
 npm i
 
-cd /Applications/MAMP/bin && ./start.sh
-cd /Applications/MAMP/bin && ./stop.sh
+rm -rf ${PATH_PROJECT}/${PATH_WP_THEME}/${PROJECT}/.git
 
-echo "CREATE DATABASE ${PROJECT};"  | /Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot
+# copy ${PATH_SCRIPT}/plugins/* ${PATH_PROJECT}/${PATH_WP_PLUGINS}/*
 
-open http://localhost:8887/${PROJECT}/
+open http://localhost:8080/wp-admin/install.php
 
-echo "The ${PROJECT} project has been initiated!"
+echo "The ${PROJECT} project has been initiated at:"
+echo "${PATH_PROJECT}"
